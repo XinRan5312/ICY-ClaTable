2943ec47d113545edea9b0fbef210052d0f55254
Zane Xu <wo1996zhi@vip.qq.com>
2016-04-16 00:51:28 +0800

    Update ServiceApiConfig.java

--- app/src/main/java/com/example/zane/icy_clatable/config/ServiceApiConfig.java
+++ app/src/main/java/com/example/zane/icy_clatable/config/ServiceApiConfig.java
@@ -5,7 +5,7 @@ package com.example.zane.icy_clatable.config;
  */
 public class ServiceApiConfig {
     //课表base url
-    public static final String BASE_URL = "http://219.153.62.77/";
+    public static final String BASE_URL = "http://xxx.xxx.xx.xx/";
     //和风天气base url
     public static final String BASE_WEATHER_URL = "https://api.heweather.com/x3/weather?cityid=CN101040100&key=f2de94fbb40e459db930df9395ac304d";
 }
